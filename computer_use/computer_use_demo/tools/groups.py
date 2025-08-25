b800b140e9555f80b2f5cc6d6d6670e0bd346658
Mihail Shinder <mihail@shinder.ml>
2025-08-25 21:53:11 +0300

    Enable descriptive vision for openai

--- computer_use/computer_use_demo/tools/groups.py
+++ computer_use/computer_use_demo/tools/groups.py
@@ -4,6 +4,7 @@ from typing import Literal
 from .base import BaseLLMTool
 from .bash import GenericBashTool, BashTool20241022, BashTool20250124
 from .computer import ComputerTool20241022, ComputerTool20250124
+from .descriptive_vision import DescriptiveVision
 from .edit import EditTool20241022, EditTool20250124, EditTool20250429
 
 ToolVersion = Literal[
@@ -39,7 +40,7 @@ TOOL_GROUPS: list[ToolGroup] = [
     ),
     ToolGroup(
         version="computer_use_openai_manual",
-        tools=[GenericBashTool],
+        tools=[GenericBashTool, DescriptiveVision],
     ),
 ]
 
