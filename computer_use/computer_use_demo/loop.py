3bf8f125a5fe9ece3562720ceb87bd4250c9906e
Mihail Shinder <mihail@shinder.ml>
2025-08-25 18:15:30 +0300

    PROMPT: Add xdotool, wmctr, remove DISPLAY=:1
    This should substitute anthropic computeruse tool,
    since it's just xdotool, but may reduce quality.
    DISPLAY demand is now redundant as set manually in bash tool

--- computer_use/computer_use_demo/loop.py
+++ computer_use/computer_use_demo/loop.py
@@ -69,7 +69,8 @@ SYSTEM_PROMPT = f"""<SYSTEM_CAPABILITY>
 * You are utilising an Ubuntu virtual machine using {platform.machine()} architecture with internet access.
 * You can feel free to install Ubuntu applications with your bash tool. Use curl instead of wget.
 * To open firefox, please just click on the firefox icon.  Note, firefox-esr is what is installed on your system.
-* Using bash tool you can start GUI applications, but you need to set export DISPLAY=:1 and use a subshell. For example "(DISPLAY=:1 xterm &)". GUI apps run with bash tool will appear within your desktop environment, but they may take some time to appear. Take a screenshot to confirm it did.
+* xdotool and wmctrl installed on your system use them to interact with gui
+* Using bash tool you can start GUI applications, but you need to use a subshell. For example "(xterm &)". GUI apps run with bash tool will appear within your desktop environment, but they may take some time to appear. Take a screenshot to confirm it did.
 * When using your bash tool with commands that are expected to output very large quantities of text, redirect into a tmp file and use str_replace_based_edit_tool or `grep -n -B <lines before> -A <lines after> <query> <filename>` to confirm output.
 * When viewing a page it can be helpful to zoom out so that you can see everything on the page.  Either that, or make sure you scroll down to see everything before deciding something isn't available.
 * When using your computer function calls, they take a while to run and send back to you.  Where possible/feasible, try to chain multiple of these calls all into one function calls request.
